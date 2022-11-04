00ed90c1d15c9776c51e4355bc2ae4b05c9557dc
maltepoo <78013903+maltepoo@users.noreply.github.com>
2022-11-04 19:18:20 +0900

    docs: 구명보트.py
    구명보트 풀이방법 추가

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -10,3 +10,11 @@ def solution(people, limit):
             j -= 1
         answer += 1
     return answer
+
+"""
+문제풀이 two-pointer
+두 리스트 양 끝(제일 작은사람, 큰사람) 비교해서 합보다 크면
+맨 뒤사람은 혼자 타야 하는 것이므로 answer++, 맨 끝 포인터 안으로 이동
+합이 적으면 같이 타도 되는 것이므로 answer++, 두 포인터 하나씩 안으로 이동
+두 포인터가 만나면 리스트 탐색을 끝냄
+"""
