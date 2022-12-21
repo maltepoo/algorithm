2a7c9b7a4d23fc74590e3f1a9513d55e69899403
maltepoo <78013903+maltepoo@users.noreply.github.com>
2022-12-21 15:51:11 +0900

    [unrated] Title: 롤케이크 자르기, Time: 682.76 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,24 @@
+# 시간초과 --------
+# def solution(topping):
+#     answer = 0
+#     for i in range(len(topping)):
+#         x, y = set(topping[:i]), set(topping[i:])
+#         if len(x) == len(y):
+#             answer += 1
+#     return answer
+from collections import Counter, deque
+def solution(topping):
+    answer = 0
+    tp = Counter(topping)
+    
+    s, e = 0, len(tp)
+    st = set()
+    
+    for t in topping:
+        if tp[t] > 0:
+            st.add(t)
+        tp[t] -= 1
+        if tp[t] == 0:
+            e -= 1
+        if len(st) == e: answer += 1
+    return answer
