c1a62676ed0e509f5f0cfe0063a2ce790b5eb633
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-03-20 23:15:23 +0900

    [Silver IV] Title: 숫자 정사각형, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split(" "))
+arr = [list(map(int, list(input()))) for _ in range(n)]
+
+res = 1
+for size in range(min(n, m), 0, -1): #가장 큰 사각형부터 거꾸로 찾음
+    for i in range(n):
+        for j in range(m):
+            num = arr[i][j] #현재 숫자(왼상단)
+            ni = i+(size-1)
+            nj = j+(size-1)
+            if 0 <= ni < n and 0 <= nj < m and num == arr[ni][j] == arr[i][nj] == arr[ni][nj]:
+                if res < size**2:
+                    res = size**2
+print(res)
