b7d647725239e8ae1d68e16f383c8d22346d07f1
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-04-20 09:50:22 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 240 ms, Memory: 41316 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,10 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-ij = [tuple(map(int, input().split())) for _ in range(m)]
+prefixSum = [0]*(n+1)
 
-prefix_sum = [0]*(n+1)
-for i in range(n):
-    prefix_sum[i+1] = arr[i] + prefix_sum[i] # 누적합
+for i in range(1, n+1):
+    prefixSum[i] = prefixSum[i-1] + arr[i-1]
 
-for i, j in ij:
-    print(prefix_sum[j]-prefix_sum[i-1]) # 구간합은 누적합에서 i 이전까지의 구간합을 뺀 것
+for _ in range(m):
+    i, j = map(int, input().split())
+    print(prefixSum[j] - prefixSum[i-1])
