bb8d96f9e083f43739b2755a14b2c3b84a3ec792
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-04-23 11:36:15 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 248 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a, b = 0, 0
+# 최대 공약수 구하기
+for i in range(min(n, m), 0, -1):
+    if (n % i == 0) and (m % i == 0):
+        a = i
+        break
+# 최소 공배수 구하기
+for j in range(max(n, m), (n * m) + 1):
+    if j % n == 0 and j % m == 0:
+        b = j
+        break
+print(a)
+print(b)
