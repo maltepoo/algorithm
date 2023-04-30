fba58f030bfbae922bfbf5f1228bd23abdacdf52
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-04-30 22:16:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4696.15 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 43.6 MB, 시간: 4199.20 ms
+메모리: 43.8 MB, 시간: 4696.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,8 +1,9 @@
 def solution(elements):
-    circled = elements + elements;
-    total = set();
+    circled = elements + elements
+    total = set()
+    total.add(sum(elements))
     
-    for i in range(1, len(elements)+1): # 연속 부분 수열의 길이
+    for i in range(1, len(elements)): # 연속 부분 수열의 길이
         for j in range(0, len(elements)):
             total.add(sum(circled[j:j+i]))
         
