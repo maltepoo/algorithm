24e6590bc356c237b165c2ff15d12dabc70807bc
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-05-22 18:55:49 +0900

    [level 3] Title: 단속카메라, Time: 2.21 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,21 +1,13 @@
 def solution(routes):
     answer = 1
-    routes.sort(key=lambda x:x[1])
-
-    """
-    틀림
+    routes.sort(key=lambda x: x[1])
     st, ed = routes[0][0], routes[0][1]
     for s, e in routes:
-        if st <= s <= ed or st <= e <= ed:
-            continue
-        else:
+        if s > ed:  # 시작점(s)이 이전 구간의 끝점(ed)보다 큰 경우
             answer += 1
-            st = min(st, s, e)
-            ed = max(ed, s, e)
-    """
-    cmr = routes[0][1]
-    for s, e in routes:
-        if s > cmr:
-            answer += 1
-            cmr = e
+            st = s
+            ed = e
+        else:  # 시작점(s)이 이전 구간의 끝점(ed)보다 작은 경우
+            st = max(st, s)
+            ed = min(ed, e)
     return answer
