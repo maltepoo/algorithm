1a6413edaebd84f3f6cc4189c90e2f56876dffbf
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-06-07 21:10:38 +0900

    [unrated] Title: 숫자 변환하기, Time: 603.42 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,3 +1,14 @@
 def solution(x, y, n):
-    answer = 0
-    return answer
+    dp = [1e9]*(y+1)
+    dp[x] = 0
+    
+    for i in range(x, y+1):
+        # bottom-up : i를 기준으로 3가지 경우의 수의 위치에 대해 최소 회수 갱신
+        if i * 3 <= y:
+            dp[i*3] = min(dp[i]+1, dp[i*3])
+        if i * 2 <= y:
+            dp[i*2] = min(dp[i]+1, dp[i*2])
+        if i + n <= y:
+            dp[i+n] = min(dp[i]+1, dp[i+n])
+            
+    return dp[y] if dp[y] < 1e9 else -1
