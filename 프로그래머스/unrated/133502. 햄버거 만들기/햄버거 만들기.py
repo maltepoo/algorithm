d659190d81412b6c2595bfda73c379adef97792b
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-07-13 22:21:36 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,13 +1,3 @@
 def solution(ingredient):
     answer = 0
-    st = []
-
-    for i in ingredient:
-        st.append(i)
-        if i == 1 and len(st) >= 4:
-            ham = st[-4:]
-            if ham == [1, 2, 3, 1]:
-                answer += 1
-                for _ in range(4):
-                    st.pop()
     return answer
