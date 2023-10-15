0a777419e716392abb40c5f0ea8cd4a7c9f93b1a
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-10-15 23:09:16 +0900

    [Gold I] Title: K번째 수, Time: 676 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,23 @@
+n = int(input())
+k = int(input())
+
+
+# 답 범위는 1 <= res <= k
+# 인덱스 k보다 큰 수가 k위치에 올 수 없기 때문
+# 임의의 x값을 설정하고 x보다 작거나 같은 원소의 개수가 k 값이랑 일치하면 정답
+def binary_search(start, end):
+    while start <= end:
+        m = (start+end)//2
+
+        cnt = 0
+        for i in range(1, n+1):
+            # i(행)으로 m(답 예상수), k보다 작은 수가 몇 개인지 체크
+            cnt += min(m//i, n)
+
+        # k보다 작은 수의 개수가 k보다 크면 더이상 탐색할 필요 없음
+        if cnt >= k:
+            end = m-1
+        else:
+            start = m+1
+    return start
+print(binary_search(1, k))
