a1f7a8939f2859dbb62de09941b21aebff05ddd2
maltepoo <78013903+maltepoo@users.noreply.github.com>
2024-02-13 23:56:18 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 788 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,4 +1,7 @@
-S = input()
+import sys
+input = sys.stdin.readline
+
+S = input().rstrip()
 q = int(input()) # 질문의 수
 
 dp = [[0]*26 for _ in range(len(S))]
@@ -6,10 +9,10 @@ dp[0][ord(S[0])-97] = 1
 
 for i in range(1, len(S)):
     dp[i] = dp[i-1].copy()
-    dp[i][ord(S[i])-97] = dp[i-1][ord(S[i])-97] + 1
+    dp[i][ord(S[i])-97] += 1
 
 for _ in range(q):
-    n, st, ed = input().split()
+    n, st, ed = input().rstrip().split()
     n, st, ed = ord(n)-97, int(st), int(ed)
     if st == 0:
         print(dp[ed][n])
