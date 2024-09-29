df90cb4a16adbcd103fcc53a109e9afbebc5cab1
maltepoo <78013903+maltepoo@users.noreply.github.com>
2024-09-29 23:36:41 +0900

    [level 1] Title: 실패율, Time: 2768.41 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 15.5 MB, 시간: 55.59 ms
+메모리: 15.1 MB, 시간: 2768.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 23:30:42
+2024년 09월 29일 23:36:39
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,23 +1,10 @@
 def solution(N, stages):
-    stages.sort()
-
-    answer = []
-    uncleared = {i: 0 for i in range(0, N+2)} # 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수(1)
-    cleared = [0 for _ in range(0, N+2)] # 스테이지에 도달한 플레이어 수(2)
-    # 실패율 = (1)÷(2)
-
-    for s in stages:
-        uncleared[int(s)] += 1
-
-    cleared[N+1] = uncleared[N+1]
-    for i in range(N, 0, -1):
-        cleared[i] = (cleared[i+1] + uncleared[i])
-
-    for j in range(1, N+1):
-        if cleared[j] != 0:
-            answer.append([j, uncleared[j] / cleared[j]])
+    answer = {}
+    people = len(stages)
+    for i in range(1, N+1):
+        if people != 0:
+            answer[i] = stages.count(i) / people
+            people -= stages.count(i)
         else:
-            answer.append([j, 0])
-
-    answer.sort(key=lambda x:x[1], reverse=True)
-    return [k[0] for k in answer]
+            answer[i] = 0
+    return sorted(answer, key=lambda x: answer[x], reverse=True)
