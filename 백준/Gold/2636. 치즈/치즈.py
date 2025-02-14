e29475cb1e8d77e695a42da93291e0936ccc553d
maltepoo <78013903+maltepoo@users.noreply.github.com>
2025-02-15 00:33:07 +0900

    [Gold IV] Title: 치즈, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,37 +1,42 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-def bfs(x, y):
-    q = [(x, y)]
+total_chz = 0
+for i in range(n):
+    total_chz += arr[i].count(1)
+
+
+def melt_chz(x, y):
     visited[x][y] = 1
+
+    qq = [(x, y)]
     melt = []
-    while q:
-        i, j = q.pop(0)
+
+    while qq:
+        i, j = qq.pop(0)
+
         for k in range(4):
             ni, nj = i + dirs[k][0], j + dirs[k][1]
             if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj]:
                 visited[ni][nj] = 1
-                if arr[ni][nj] == 1:  # 녹일 치즈
+                if arr[ni][nj] == 1:
                     melt.append((ni, nj))
-                else: # 공기(0)이면 계속탐색
-                    q.append((ni, nj))
+                elif arr[ni][nj] == 0:
+                    qq.append((ni, nj))
     for mi, mj in melt:
         arr[mi][mj] = 0
+
     return len(melt)
 
-n, m = map(int, input().split(" "))
-arr = []
-cheese = 0
-for i in range(n):
-    temp = list(map(int, input().split(" ")))
-    arr.append(temp)
-    cheese += sum(temp)
 
-bfsCnt = 0
-while True:
-    visited = [[0] * m for _ in range(n)]
-    bfsCnt += 1
-    melted = bfs(0, 0)
-    cheese -= melted
-    if cheese == 0:
-        print(bfsCnt, melted, sep="\n")
+for sec in range(1, 1001):
+    visited = [[0]*m for _ in range(n)]
+
+    melted = melt_chz(0, 0)
+    total_chz -= melted
+
+    if total_chz == 0:
+        print(sec)
+        print(melted)
         break
